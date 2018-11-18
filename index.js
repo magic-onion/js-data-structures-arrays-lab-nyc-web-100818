68926159ee0c8a35e5bf58dc068f664c0ec1c46e
magic-onion <alexander.m.truong@gmail.com>
2018-11-17 20:26:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  return [drivers[0], drivers[1], drivers[2], name];
+}
+
+function prependDriver(name) {
+  return [name, drivers[0], drivers[1], drivers[2]]
+}
+
+function removeLastDriver() {
+  return [drivers[0], drivers[1]];
+}
+
+function removeFirstDriver() {
+  return [drivers[1], drivers[2]];
+}
